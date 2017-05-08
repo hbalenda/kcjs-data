fa1fa663c0e002df3d6a8b7a8ce36e7c2f89f4b3
hcbviolet <hviolet@thestranger.com>
2017-05-07 23:23:05 -0700

    Add totals and description

--- app/controllers/graph_controller.rb
+++ app/controllers/graph_controller.rb
@@ -1,11 +1,5 @@
-def log(message)
-    Rails.logger.info(message)
-    puts message
-end
-
 class GraphController < ApplicationController
   def index
-    # GENDER
     @genders = Entry.all.pluck(:gender).uniq
     @selected_gender = params[:gender]
 
@@ -15,7 +9,25 @@ class GraphController < ApplicationController
     @offenses = Entry.all.pluck(:offense).uniq
     @selected_offense = params[:offense]
 
-    @entries = Entry.where("gender LIKE (?) AND offense LIKE (?) AND race LIKE (?)", "#{@selected_gender}%", "%#{@selected_offense}%", "%#{@selected_race}%")
+    if params[:dv].empty?
+      @selected_dv = "%"
+    else
+      @selected_dv = to_boolean(params[:dv])
+    end
+
+    @years = Entry.all.pluck(:year).uniq
+    @selected_start_year = params[:start_year] ? params[:start_year] : 2010
+    @selected_end_year = params[:end_year] ? params[:end_year] : 2011
+
+    @entries = Entry.where("gender LIKE (?) AND offense LIKE (?) AND race LIKE (?) AND dv LIKE (?) AND year >= (?) AND year <= (?)", "#{@selected_gender}%", "%#{@selected_offense}%", "%#{@selected_race}", @selected_dv, @selected_start_year, @selected_end_year).order(race: :asc, offense: :asc, details: :asc, dv: :desc, year: :asc)
     @count = @entries.count
+    @total = Entry.where("year >= (?) AND year <= (?)", @selected_start_year, @selected_end_year).count
+  end
+  def to_boolean(str)
+    str == 'true'
+  end
+  def log(message)
+      Rails.logger.info(message)
+      puts message
   end
 end
